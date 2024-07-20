e3ff0bca3afd219fbfc6e371279c407a8e7de08b
vamastah <szymorawski@gmail.com>
2024-07-20 10:00:53 +0200

    Updated delay values for configuring GPIO pins in setup and loop functions.

--- src/gpio-test/gpio-test.ino
+++ src/gpio-test/gpio-test.ino
@@ -1,12 +1,10 @@
-#define CONFIGURE_DELAY 100
-#define INTER_GPIO_DELAY 50
+#define CONFIGURE_DELAY 1000
+#define INTER_GPIO_DELAY 5000
 
 void setup() {
   // put your setup code here, to run once:
   for (int pin = 0; pin <= 13; pin++) {
     pinMode(pin, INPUT);
-    delay(INTER_GPIO_DELAY); // Additional delay between configuring each GPIO
-    delay(INTER_GPIO_DELAY); // Additional delay between configuring each GPIO
   }
   for (int pin = A0; pin <= A5; pin++) {
     pinMode(pin, INPUT);
@@ -33,8 +31,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   for (int pin = 0; pin <= 13; pin++) {
     configureGPIO(pin);
+    delay(INTER_GPIO_DELAY);
   }
   for (int pin = A0; pin <= A5; pin++) {
     configureGPIO(pin);
+    delay(INTER_GPIO_DELAY);
   }
 }
